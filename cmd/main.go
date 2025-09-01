5fef31b37dfc5f426fc07a9f1965d1134969a222
marhiru <marcosgamern@gmail.com>
2025-08-31 23:21:27 -0300

    fix: fixing particles rendering

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,43 @@
-package particles
+package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	particles "particles-system/ascii"
+	"runtime"
+	"time"
 )
 
+func clearScreen() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
+
+	if runtime.GOOS != "windows" {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func main() {
-	coffee := NewCoffee(10)
-	for _, p := range coffee.particles {
-		fmt.Println(p.x, p.y)
+	width, height := 20, 10
+	coffeeSystem := particles.NewCoffeeSystem(width, height)
+
+	coffeeSystem.Start()
+
+	for {
+		clearScreen()
+		coffeeSystem.Update()
+
+		frame := coffeeSystem.Print()
+		for _, row := range frame {
+			fmt.Println(string(row))
+		}
+
+		time.Sleep(500 * time.Millisecond)
 	}
 }
